e126628940556b25b493dda568c18e9aabac16e1
Josh F <fowlerjosh93@gmail.com>
2019-08-28 18:32:22 -0400

    linked js to HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,12 @@ var losses = 0;
 
 var maxWrong = 11;
 
+var guessDisplayLetters = document.getElementById("guess-display-letters");
+var guessedLetters = document.getElementById("guessed-letters");
+var wrongCount = document.getElementById("wrongCount");
+var wins = document.getElementById("wins");
+var losses = document.getElementById("losses");
+
 var validLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 function keanuFilms() {
